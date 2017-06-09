a1d5d4d77f1f1b82dabd78804c0de10c896b506d
Hapachino <erickwbrownfield@gmail.com>
2017-06-09 04:02:43 -0700

    revised spacecraft filter for empty value

--- server.js
+++ server.js
@@ -31,40 +31,6 @@ app.get('/', function (req, res, next) {
     });
 });
 
-app.get('/filter-spacecraft', function (req, res, next) {
-  var context = {};
-  mysql.pool.query('SELECT s.id, s.name, classification, p.name AS program, startDate, endDate FROM spacecraft s \
-                    LEFT JOIN program p ON s.pid = p.id \
-                    WHERE s.pid = ?', [req.query.pid], function (err, results, fields) {
-      if (err) { return next(err); }
-      for (var i = 0; i < results.length; ++i) {
-        if (results[i].startDate == ("0000-00-00")) {
-          results[i].startDate = "";
-        }
-        if (results[i].endDate == "0000-00-00") {
-          results[i].endDate = "Ongoing";
-        }
-      }
-      context.results = results;
-
-      mysql.pool.query('SELECT id, name FROM program', function (err, results, fields) {
-        if (err) { return next(err); }
-        context.program = results;
-        res.render('home', context);
-      });
-    });
-});
-
-app.get('/rockets', function (req, res, next) {
-  var context = {};
-  mysql.pool.query('SELECT * from rocket \
-                    ORDER BY model ASC', function (err, results, fields) {
-      if (err) { return next(err); }
-      context.results = results;
-      res.render('rockets', context);
-    });
-});
-
 app.get('/insert-spacecraft', function (req, res, next) {
   mysql.pool.query('SELECT EXISTS(SELECT 1 FROM spacecraft WHERE name = ?) AS COUNT', [req.query.name], function (err, results, fields) {
     if (results[0].COUNT) {
@@ -80,6 +46,61 @@ app.get('/insert-spacecraft', function (req, res, next) {
       } else {
         mysql.pool.query('INSERT INTO spacecraft (name, classification, startDate, endDate) VALUES (?, ?, ?, ?)',
           [req.query.name, req.query.classification, req.query.startDate, req.query.endDate], function (err, results, fields) {
+            if (err) { return next(err); }
+            res.send(JSON.stringify(results));
+          });
+      }
+    }
+  });
+});
+
+app.get('/filter-spacecraft', function (req, res, next) {
+  var context = {};
+  if (!req.query.pid) {
+    res.redirect(302, "/");
+  } else {
+    mysql.pool.query('SELECT s.id, s.name, classification, p.name AS program, startDate, endDate FROM spacecraft s \
+                    LEFT JOIN program p ON s.pid = p.id \
+                    WHERE s.pid = ?', [req.query.pid], function (err, results, fields) {
+        if (err) { return next(err); }
+        for (var i = 0; i < results.length; ++i) {
+          if (results[i].startDate == ("0000-00-00")) {
+            results[i].startDate = "";
+          }
+          if (results[i].endDate == "0000-00-00") {
+            results[i].endDate = "Ongoing";
+          }
+        }
+        context.results = results;
+
+        mysql.pool.query('SELECT id, name FROM program', function (err, results, fields) {
+          if (err) { return next(err); }
+          context.program = results;
+          res.render('home', context);
+        });
+      });
+  }
+});
+
+app.get('/rockets', function (req, res, next) {
+  var context = {};
+  mysql.pool.query('SELECT * from rocket \
+                    ORDER BY model ASC', function (err, results, fields) {
+      if (err) { return next(err); }
+      context.results = results;
+      res.render('rockets', context);
+    });
+});
+
+app.get('/insert-rockets', function (req, res, next) {
+  mysql.pool.query('SELECT EXISTS(SELECT 1 FROM rocket WHERE model = ?) AS COUNT', [req.query.model], function (err, results, fields) {
+    if (results[0].COUNT) {
+      res.send("dup");
+      return;
+    } else {
+      if (req.query.pid) {
+        mysql.pool.query('INSERT INTO rocket (model, height, stages, thrust) VALUES (?, ?, ?, ?)',
+          [req.query.model, req.query.height, req.query.stages, req.query.thrust], function (err, results, fields) {
             if (err) { return next(err); }
             res.send(JSON.stringify(results));
           });
